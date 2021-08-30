dacb05e25fae9ec989472c37a926a66891497212
Noe Mora <noe_cipo78@hotmail.com>
2021-08-30 10:10:01 -0300

    added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+
+import HomePage from './homepage.component';
 
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }
